2c4f95d46933dc420f21a398a668cbb8f3c7678c
Guilherme Freire <guilhermebfreire@gmail.com>
2016-07-25 21:23:38 -0300

    + SQL button + Queries to be made + Scroll events + Line Chart + Respective styles

---
+++ queries.sql
@@ -0,0 +1,7 @@
+-- Chart 1
+SELECT ano, valor
+	FROM aposentadoria
+	WHERE valor > 0
+	ORDER BY ano;
+
+--Chart 2
--- script.js
+++ script.js
@@ -1,40 +1,58 @@
+var last_known_scroll_position = 0;
+var ticking = false;
+var top_bar_active = false;
+var topBar = document.getElementById("top_bar");
+var threshold = 420;
+
+function doSomething(scroll_pos) {
+  if(scroll_pos > threshold && !top_bar_active){
+  	top_bar_active = true;
+  	topBar.className = "";
+  }
+  if(scroll_pos < threshold && top_bar_active){
+  	top_bar_active = false;
+  	topBar.className = "hide";
+  }
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+  }
+  ticking = true;
+});
+
+
+//Chart related javascript
 var canvas1 = document.getElementById("chart1");
 var ctxChart1 = canvas1.getContext("2d");
 
-var chart1 = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+var data1 = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+            label: 'Scatter Dataset',
+            data: [
+            		{x:1996, y:12048},
+            		{x:1997, y:13947},
+            		{x:1998, y:29477}
+            	  ]
         }]
-    },
+};
+
+
+var chart1 = new Chart(ctxChart1, {
+    type: 'line',
+    data: data1,
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
             }]
-        },
-        maintainAspectRatio: true
+        }
     }
 });
